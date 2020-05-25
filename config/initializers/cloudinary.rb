9a21d164b581ae9a0a9734bd9a16563f91eeeb2d
creyes-dev <creyes.dev@gmail.com>
2020-05-25 13:44:15 -0700

    configurar Cloudinary con CarrierWave

---
+++ config/initializers/cloudinary.rb
@@ -0,0 +1,7 @@
+Cloudinary.config do |config|
+    config.cloud_name = "Ducgqjeli"
+    config.api_key = "545947936916264"
+    config.api_secret = "CfHA6f5bf99DBrDcC_luuxe8o9Q"
+    config.secure = true
+    config.cdn_subdomain = true
+end
